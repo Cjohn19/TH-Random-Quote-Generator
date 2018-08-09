6a86960ee5f426078172883be374206023415d99
Christopher Johnson <cjhondo@gmail.com>
2018-08-09 15:19:34 -0400

    Added "quotes" array

--- js/script.js
+++ js/script.js
@@ -2,6 +2,45 @@
 
 // Create the array of quote objects and name it quotes
 
+var quotes = [
+  {
+     quote: "The answer is: me and dead owls don't give a hoot.",
+     source: 'Raylan Givens',
+     citation: 'Justified',
+     year: '2012'
+  },
+  {
+    quote: "I solemnly swear I am up to no good.",
+    source: 'Harry Potter',
+    citation: 'Harry Potter and the Prisoner of Azkaban',
+    year: 'September, 1999'
+  },
+  {
+    quote: "You come at the king, you best not miss.",
+    source: 'Omar Little',
+    citation: 'The Wire',
+    year: 'June, 2002'
+  },
+  {
+    quote: "Rubbing alcohol is for outside wounds, drinking alcohol is for inside wounds. It's science"
+    source: 'Nick Miller',
+    citation: 'New Girl',
+    year:''
+  },
+  {
+    quote: "You gave me a cookie, I gave you a cookie. You gave me cookie gave you cookie. Gave me cookie got you cookie! You gave me cookie I got you a cookie man! Gave me cookie got you cookie, we're even! We're even, Schmidt!"
+    source: 'Nick Miller',
+    citation: 'New Girl',
+    year:''
+  },
+  {
+    quote: "Either thou or I, or both, must go with him",
+    source: 'Romeo',
+    citation: 'Rome & Juliet By William Shakespeare',
+    year: "1597"
+  }
+];
+
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
