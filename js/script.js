59277a96a6db138fec6145c553e7b25a1716fef2
Christopher Johnson <cjhondo@gmail.com>
2018-08-10 11:17:45 -0400

    Added set interval

--- js/script.js
+++ js/script.js
@@ -76,3 +76,6 @@ function createHTMLForQuote(quoteObj){
 
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//
+setInterval(printQuote, 30000);
